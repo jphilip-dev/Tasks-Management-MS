d3a6a50769a2f127975595cdeb6ef51936b66fc2
jphilip-dev <john.p.s.de.roxas+git@gmail.com>
2025-05-19 16:27:50 +0800

    Add user-service module

---
+++ user-service/src/test/java/com/jphilip/tm/user/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jphilip.tm.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
