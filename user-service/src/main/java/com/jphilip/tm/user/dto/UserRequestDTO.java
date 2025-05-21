713a191712008698a70588cbfd72b0dc3f3b6605
jphilip-dev <john.p.s.de.roxas+git@gmail.com>
2025-05-22 00:30:46 +0800

    Add Validation for the size of the password

--- user-service/src/main/java/com/jphilip/tm/user/dto/UserRequestDTO.java
+++ user-service/src/main/java/com/jphilip/tm/user/dto/UserRequestDTO.java
@@ -2,6 +2,7 @@ package com.jphilip.tm.user.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,6 +21,7 @@ public class UserRequestDTO {
     private String name;
 
     @NotBlank
+    @Size(min = 8, message = "Password must at least 8 characters long")
     private String password;
 
     private Long teamLeadId;
