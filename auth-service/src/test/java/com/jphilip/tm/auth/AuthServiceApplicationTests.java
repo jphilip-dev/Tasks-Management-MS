93bc7d9608156a99772f47e0deb2f67a82321a92
jphilip-dev <john.p.s.de.roxas+git@gmail.com>
2025-05-21 11:48:25 +0800

    Add auth service module

---
+++ auth-service/src/test/java/com/jphilip/tm/auth/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jphilip.tm.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
